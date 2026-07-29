ffd131b63a8ba56b6dddf22fbf442cbf2b4ac16f
Ulysses <ulyssesburdeniii@gmail.com>
2026-07-29 19:20:51 -0400

    chore: initalize Atlas with Vite and core dependencies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Atlas</h1>
+      <p className="lead">Your personal operating system.</p>
+
+      <button className="btn btn-primary">
+        Bootstrap Works
+      </button>
+    </div>
+  );
+}
+
+export default App;
